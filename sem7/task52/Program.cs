990406aec7e3cf657fb349e5f812df54710318e5
Алина Прескура <a.preskura@logic-way.local>
2022-07-24 20:37:14 +0300

    task52

---
+++ sem7/task52/Program.cs
@@ -0,0 +1,40 @@
+﻿// // Задайте двумерный массив из целых чисел. 
+// // Найдите среднее арифметическое элементов в каждом столбце.
+namespace Zadacha52
+{
+   class Zadachi
+    {
+         static void Main(string[] args)
+        {
+             string[,] massiv = new string[5, 5];
+             double result = 0;
+                for (int i = 0; i < 5; i++)
+                {
+                    for (int j = 0; j < 5; j++)
+                    {
+                        massiv[i,j] = Convert.ToString(new Random().Next(1,10));
+                        Console.Write($" {massiv[i,j]} ");
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+                int k = 0;
+                int l = 0;
+                int n = 1;
+               while (k < 5 | l < 5)
+               {
+                 while (k < 5)
+                 {
+                    result += Convert.ToDouble(massiv[k,l]);
+                    k++;
+                 }
+                 Console.WriteLine($"Среднее арифметическое стобца {n} = {result/5}");
+                 result = 0;
+                 k = 0;
+                 l++;
+                 n++;
+               }
+               Console.WriteLine();
+         }
+     }  
+}   
