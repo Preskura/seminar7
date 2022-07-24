736d764b9dfad8d1013dba405d956c7313058d73
Алина Прескура <a.preskura@logic-way.local>
2022-07-24 20:34:56 +0300

    task47

---
+++ sem7/task47/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+namespace Zadacha47
+{
+   class Zadachi
+    {
+         static void Main(string[] args)
+        {
+             Console.WriteLine("Введите значение строк m: ");
+             int m = Convert.ToInt32(Console.ReadLine());
+             Console.WriteLine("Введите значение столбцов n: ");
+             int n = Convert.ToInt32(Console.ReadLine());
+             double [,] massiv = new double [m, n];
+                for (int i = 0; i < m; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        massiv[i,j] = new Random().NextDouble()*10;
+                        Console.Write($" {massiv[i,j]} ");
+                    }
+                    Console.WriteLine();
+                }
+         }
+     }  
+}   
